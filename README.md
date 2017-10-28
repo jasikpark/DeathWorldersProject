92702746c7b4307ef3b25e66fd65ce1247f907a2
Caleb Jasik <cable023@gmail.com>
2017-10-27 21:56:01 -0500

    tada

---
+++ README.md
@@ -0,0 +1,3 @@
+# This is my attempt at making DeathWorlders available offline.
+
+Use crowbook to build the books.
--- runthis.py
+++ runthis.py
@@ -1,8 +1,18 @@
 import requests
+import os
+import errno
 
 # implement input arguments later. for now, i'll just hardcode the file.
 
 f = open('deathworlders-url-list-2.csv','r')
+
+try:
+    os.makedirs('chapters/')
+except OSError as exc:
+    if exc.error == errno.EEXIST and os.path.isdir('chapters/'):
+        pass
+    else:
+        raise
 
 urls = f.readlines()
 
@@ -14,7 +24,7 @@ for i,x in enumerate(fixed_urls):
     r = requests.get(first_url + x)
     print r.url + "\n"
     name = x.rsplit('/', 1)[-1].strip()
-    filename = str(i+100) + "_" + name + ".md"
+    filename = "chapters/" + str(i+100) + "_" + name + ".md"
     print "writing to " + filename + "\n"
     f = open(filename,'w')
     f.write(r.content)
