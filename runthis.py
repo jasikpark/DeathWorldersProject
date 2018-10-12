bb3a6d693bc2ab828d0d95f9cbf008fe155fa8c7
Caleb Jasik <calebjasik@jasik.xyz>
2018-10-12 00:45:20 -0500

    using the yapf formatter for python

--- runthis.py
+++ runthis.py
@@ -4,7 +4,7 @@ import errno
 
 # implement input arguments later. for now, i'll just hardcode the file.
 
-f = open('deathworlders-url-list-2.csv','r')
+f = open('deathworlders-url-list-2.csv', 'r')
 
 try:
     os.makedirs('chapters/')
@@ -20,13 +20,13 @@ fixed_urls = [x.strip('\r\n') for x in urls]
 
 first_url = """http://markdownplease.com/?url="""
 
-for i,x in enumerate(fixed_urls):
+for i, x in enumerate(fixed_urls):
     r = requests.get(first_url + x)
     print r.url + "\n"
     name = x.rsplit('/', 1)[-1].strip()
-    filename = "chapters/" + str(i+100) + "_" + name + ".md"
+    filename = "chapters/" + str(i + 100) + "_" + name + ".md"
     print "writing to " + filename + "\n"
-    f = open(filename,'w')
+    f = open(filename, 'w')
     f.write(r.content)
 
 print "All webpages have been downloaded and stored as markdown files!"
